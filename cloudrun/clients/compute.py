a80c5d8f2cd3516a09276b04090eaa8fc556e6f0
Rovalinks AI Works <rovalinksit@gmail.com>
2026-07-16 13:13:10 +0100

    Comment out unused client initializations
    Comment out unused clients in the compute.py file to streamline the code and improve readability.

--- cloudrun/clients/compute.py
+++ cloudrun/clients/compute.py
@@ -12,10 +12,10 @@ class ComputeClient(ResourceClient):
         ("instances", "zones"): {"asset_type": "compute.googleapis.com/Instance", "client_attr": "instances", "get_arg": "instance", "set_labels_request_cls": compute_v1.InstancesSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "instances_set_labels_request_resource"},
         ("disks", "zones"): {"asset_type": "compute.googleapis.com/Disk", "client_attr": "disks", "get_arg": "disk", "set_labels_request_cls": compute_v1.ZoneSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "zone_set_labels_request_resource"},
         ("disks", "regions"): {"asset_type": "compute.googleapis.com/Disk", "client_attr": "region_disks", "get_arg": "disk", "set_labels_request_cls": compute_v1.RegionSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "region_set_labels_request_resource"},
-        ("addresses", "regions"): {"asset_type": "compute.googleapis.com/Address", "client_attr": "addresses", "get_arg": "address", "set_labels_request_cls": compute_v1.RegionSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "region_set_labels_request_resource"},
-        ("addresses", "global"): {"asset_type": "compute.googleapis.com/Address", "client_attr": "global_addresses", "get_arg": "address", "set_labels_request_cls": compute_v1.GlobalSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "global_set_labels_request_resource"},
-        ("forwardingRules", "regions"): {"asset_type": "compute.googleapis.com/ForwardingRule", "client_attr": "forwarding_rules", "get_arg": "forwarding_rule", "set_labels_request_cls": compute_v1.RegionSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "region_set_labels_request_resource"},
-        ("forwardingRules", "global"): {"asset_type": "compute.googleapis.com/ForwardingRule", "client_attr": "global_forwarding_rules", "get_arg": "forwarding_rule", "set_labels_request_cls": compute_v1.GlobalSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "global_set_labels_request_resource"},
+        # ("addresses", "regions"): {"asset_type": "compute.googleapis.com/Address", "client_attr": "addresses", "get_arg": "address", "set_labels_request_cls": compute_v1.RegionSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "region_set_labels_request_resource"},
+        # ("addresses", "global"): {"asset_type": "compute.googleapis.com/Address", "client_attr": "global_addresses", "get_arg": "address", "set_labels_request_cls": compute_v1.GlobalSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "global_set_labels_request_resource"},
+        # ("forwardingRules", "regions"): {"asset_type": "compute.googleapis.com/ForwardingRule", "client_attr": "forwarding_rules", "get_arg": "forwarding_rule", "set_labels_request_cls": compute_v1.RegionSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "region_set_labels_request_resource"},
+        # ("forwardingRules", "global"): {"asset_type": "compute.googleapis.com/ForwardingRule", "client_attr": "global_forwarding_rules", "get_arg": "forwarding_rule", "set_labels_request_cls": compute_v1.GlobalSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "global_set_labels_request_resource"},
         
         # ("networkEndpointGroups", "zones"): {"asset_type": "compute.googleapis.com/NetworkEndpointGroup", "client_attr": "network_endpoint_groups", "get_arg": "network_endpoint_group", "set_labels_request_cls": compute_v1.ZoneSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "zone_set_labels_request_resource"},
         # ("instanceGroups", "zones"): {"asset_type": "compute.googleapis.com/InstanceGroup", "client_attr": "instance_groups", "get_arg": "instance_group", "set_labels_request_cls": compute_v1.ZoneSetLabelsRequest, "set_labels_method": "set_labels", "set_labels_arg_name": "zone_set_labels_request_resource"},
@@ -46,62 +46,75 @@ class ComputeClient(ResourceClient):
     SUPPORTED_LABEL_TYPES = {meta["asset_type"] for meta in REGISTRY.values() if meta["set_labels_request_cls"] is not None}
     
     def __init__(self):
+        # 1. Operational clients (kept at top)
         self.zone_operations = compute_v1.ZoneOperationsClient()
         self.region_operations = compute_v1.RegionOperationsClient()
         self.global_operations = compute_v1.GlobalOperationsClient()
+
+        # 2. Clients ordered by their appearance in the REGISTRY
         self.instances = compute_v1.InstancesClient()
-        self.resource_policies = compute_v1.ResourcePoliciesClient()
         self.disks = compute_v1.DisksClient()
         self.region_disks = compute_v1.RegionDisksClient()
-        self.snapshots = compute_v1.SnapshotsClient()
-        self.images = compute_v1.ImagesClient()
-        self.machine_images = compute_v1.MachineImagesClient()
-        self.networks = compute_v1.NetworksClient()
-        self.subnetworks = compute_v1.SubnetworksClient()
-        self.firewalls = compute_v1.FirewallsClient()
-        self.addresses = compute_v1.AddressesClient()
-        self.global_addresses = compute_v1.GlobalAddressesClient()
-        self.forwarding_rules = compute_v1.ForwardingRulesClient()
-        self.routers = compute_v1.RoutersClient()
-        self.network_attachments = compute_v1.NetworkAttachmentsClient()
-        self.service_attachments = compute_v1.ServiceAttachmentsClient()
-        self.vpn_gateways = compute_v1.VpnGatewaysClient()
-        self.vpn_tunnels = compute_v1.VpnTunnelsClient()
-        self.target_vpn_gateways = compute_v1.TargetVpnGatewaysClient()
-        self.external_vpn_gateways = compute_v1.ExternalVpnGatewaysClient()
-        self.packet_mirrorings = compute_v1.PacketMirroringsClient()
-        self.target_pools = compute_v1.TargetPoolsClient()
-        self.ssl_certificates = compute_v1.SslCertificatesClient()
-        self.region_ssl_certificates = compute_v1.RegionSslCertificatesClient()
-        self.ssl_policies = compute_v1.SslPoliciesClient()
-        self.target_http_proxies = compute_v1.TargetHttpProxiesClient()
-        self.region_target_http_proxies = compute_v1.RegionTargetHttpProxiesClient()
-        self.target_https_proxies = compute_v1.TargetHttpsProxiesClient()
-        self.region_target_https_proxies = compute_v1.RegionTargetHttpsProxiesClient()
-        self.url_maps = compute_v1.UrlMapsClient()
-        self.region_url_maps = compute_v1.RegionUrlMapsClient()
-        self.backend_services = compute_v1.BackendServicesClient()
-        self.region_backend_services = compute_v1.RegionBackendServicesClient()
-        self.health_checks = compute_v1.HealthChecksClient()
-        self.region_health_checks = compute_v1.RegionHealthChecksClient()
-        self.network_endpoint_groups = compute_v1.NetworkEndpointGroupsClient()
-        self.instance_groups = compute_v1.InstanceGroupsClient()
-        self.region_instance_groups = compute_v1.RegionInstanceGroupsClient()
-        self.instance_templates = compute_v1.InstanceTemplatesClient()
-        self.instance_group_managers = compute_v1.InstanceGroupManagersClient()
-        self.region_instance_group_managers = compute_v1.RegionInstanceGroupManagersClient()
-        self.security_policies = compute_v1.SecurityPoliciesClient()
+        # self.addresses = compute_v1.AddressesClient()
+        # self.global_addresses = compute_v1.GlobalAddressesClient()
+        # self.forwarding_rules = compute_v1.ForwardingRulesClient()
+        # self.global_forwarding_rules = compute_v1.GlobalForwardingRulesClient() 
+        
+        # # Added (was in REGISTRY but missing in original __init__)
+        # self.network_endpoint_groups = compute_v1.NetworkEndpointGroupsClient()
+        # self.instance_groups = compute_v1.InstanceGroupsClient()
+        # self.resource_policies = compute_v1.ResourcePoliciesClient()
+        # self.subnetworks = compute_v1.SubnetworksClient()
+        # self.region_health_checks = compute_v1.RegionHealthChecksClient()
+        # self.region_backend_services = compute_v1.RegionBackendServicesClient()
+        # self.region_ssl_certificates = compute_v1.RegionSslCertificatesClient()
+        # self.region_target_https_proxies = compute_v1.RegionTargetHttpsProxiesClient()
+        # self.region_url_maps = compute_v1.RegionUrlMapsClient()
+        # self.region_target_http_proxies = compute_v1.RegionTargetHttpProxiesClient()
+        # self.region_instance_groups = compute_v1.RegionInstanceGroupsClient()
+        # self.vpn_tunnels = compute_v1.VpnTunnelsClient()
+        # self.vpn_gateways = compute_v1.VpnGatewaysClient()
+        # self.health_checks = compute_v1.HealthChecksClient()
+        # self.backend_services = compute_v1.BackendServicesClient()
+        # self.ssl_certificates = compute_v1.SslCertificatesClient()
+        # self.target_https_proxies = compute_v1.TargetHttpsProxiesClient()
+        # self.url_maps = compute_v1.UrlMapsClient()
+        # self.snapshots = compute_v1.SnapshotsClient()
+        # self.target_http_proxies = compute_v1.TargetHttpProxiesClient()
+        # self.machine_images = compute_v1.MachineImagesClient()
+        # self.images = compute_v1.ImagesClient()
+        # self.external_vpn_gateways = compute_v1.ExternalVpnGatewaysClient()
+        # self.networks = compute_v1.NetworksClient()
+
+        # 3. Additional clients present in original __init__ but not in REGISTRY
+        # self.firewalls = compute_v1.FirewallsClient()
+        # self.routers = compute_v1.RoutersClient()
+        # self.network_attachments = compute_v1.NetworkAttachmentsClient()
+        # self.service_attachments = compute_v1.ServiceAttachmentsClient()
+        # self.target_vpn_gateways = compute_v1.TargetVpnGatewaysClient()
+        # self.packet_mirrorings = compute_v1.PacketMirroringsClient()
+        # self.target_pools = compute_v1.TargetPoolsClient()
+        # self.ssl_policies = compute_v1.SslPoliciesClient()
+        # self.instance_templates = compute_v1.InstanceTemplatesClient()
+        # self.instance_group_managers = compute_v1.InstanceGroupManagersClient()
+        # self.region_instance_group_managers = compute_v1.RegionInstanceGroupManagersClient()
+        # self.security_policies = compute_v1.SecurityPoliciesClient()
 
     def supports(self, a): return a.startswith("compute.googleapis.com/")
     
     def supports_labels(self, a): return a in self.SUPPORTED_LABEL_TYPES
     
     def _parse_resource_url(self, u):
-        if u.startswith("//compute.googleapis.com/"): u = u[len("//compute.googleapis.com/"):]
-        p = u.strip("/").split("/")
-        proj, scope = p[1], p[2]
-        if scope in ("zones", "regions"): return {"project": proj, "scope_type": scope, "scope_value": p[3], "resource_type": p[4], "name": p[5]}
-        return {"project": proj, "scope_type": "global", "scope_value": "global", "resource_type": p[3], "name": p[4]}
+            if "//compute.googleapis.com/" in u: u = u.split("//compute.googleapis.com/")[1]
+            p = u.strip("/").split("/")
+            # Expected format: projects/{project}/{scope_type}/{scope_value}/{resource_type}/{name}
+            return {
+                "project": p[1], 
+                "scope_type": p[2], 
+                "scope_value": p[3], 
+                "resource_type": p[4], 
+                "name": p[5]
+            }
     
     def labels(self, r: Resource):
         try:
