2b09fbc7408cfcabfaec7fe8ddf4ee82c36419dd
Rohith <rohith.singirikonda@rovalinks.com>
2026-07-16 14:10:34 +0100

    Added _last_refresh tracking to compare against REGISTRY_CACHE_TTL reader.py

--- cloudrun/registry/reader.py
+++ cloudrun/registry/reader.py
@@ -1,96 +1,35 @@
-from io import StringIO
-
+import time
 import yaml
+from io import StringIO
 from google.cloud import storage
-
 from cache.registry_cache import RegistryCache
-from config import REGISTRY_BUCKET, REGISTRY_PREFIX
+from config import REGISTRY_BUCKET, REGISTRY_PREFIX, REGISTRY_CACHE_TTL
 from utils.logger import logger
 
-
 class RegistryReader:
-    """
-    Reads application registry YAML files
-    from Cloud Storage.
-    """
-
+    """Reads application registry YAML files from Cloud Storage."""
     def __init__(self):
-
         self.client = storage.Client()
+        self._last_refresh = 0
 
     def load_all(self):
-
+        current_time = time.time()
         applications = RegistryCache.get()
-
-        if applications is not None:
+        if applications is not None and (current_time - self._last_refresh) < REGISTRY_CACHE_TTL:
             return applications
-
-        logger.info(
-            "========== REGISTRY READER START =========="
-        )
-
-        logger.info(
-            "Bucket: %s",
-            REGISTRY_BUCKET,
-        )
-
-        logger.info(
-            "Prefix: %s",
-            REGISTRY_PREFIX,
-        )
-
+        logger.info("========== REGISTRY READER START ==========")
+        logger.info("Bucket: %s | Prefix: %s", REGISTRY_BUCKET, REGISTRY_PREFIX)
         applications = []
-
-        blobs = list(
-
-            self.client.list_blobs(
-
-                REGISTRY_BUCKET,
-
-                prefix=REGISTRY_PREFIX,
-
-            )
-
-        )
-
-        logger.info(
-            "Blob count: %d",
-            len(blobs),
-        )
-
+        blobs = list(self.client.list_blobs(REGISTRY_BUCKET, prefix=REGISTRY_PREFIX))
+        logger.info("Blob count: %d", len(blobs))
         for blob in blobs:
-
             if not blob.name.endswith(".yaml"):
                 continue
-
-            logger.info(
-                "Blob: %s",
-                blob.name,
-            )
-
-            applications.append(
-
-                yaml.safe_load(
-
-                    StringIO(
-
-                        blob.download_as_text()
-
-                    )
-
-                )
-
-            )
-
-        logger.info(
-            "Applications loaded: %d",
-            len(applications),
-        )
-
-        logger.info(
-            "========== REGISTRY READER END =========="
-        )
-
+            logger.info("Blob: %s", blob.name)
+            applications.append(yaml.safe_load(StringIO(blob.download_as_text())))
+        logger.info("Applications loaded: %d", len(applications))
+        logger.info("Registry cache refreshed. Expires in %s seconds.", REGISTRY_CACHE_TTL)
+        logger.info("========== REGISTRY READER END ==========")
         RegistryCache.set(applications)
-
+        self._last_refresh = current_time
         return applications
