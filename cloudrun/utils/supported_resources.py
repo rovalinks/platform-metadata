ebaa811f0913312e433f6be6c467090c239549b6
Rohith <rohith.singirikonda@rovalinks.com>
2026-07-16 01:39:13 +0100

    added all compute supported resources

--- cloudrun/utils/supported_resources.py
+++ cloudrun/utils/supported_resources.py
@@ -1,31 +1,57 @@
 SUPPORTED_LABEL_RESOURCES = {
-    "compute.googleapis.com/Instance",
-    "compute.googleapis.com/Disk",
-    # "compute.googleapis.com/Image",
-    # "compute.googleapis.com/Snapshot",
-    # "compute.googleapis.com/Address",
-    # "compute.googleapis.com/ForwardingRule",
-    # "compute.googleapis.com/InstanceTemplate",
-    "storage.googleapis.com/Bucket",
-    "bigquery.googleapis.com/Dataset",
-    "sqladmin.googleapis.com/Instance",
-    "artifactregistry.googleapis.com/Repository",
+    "compute.googleapis.com/Subnetwork"
+    "compute.googleapis.com/HealthCheck"
+    "compute.googleapis.com/NetworkEndpointGroup"
+    "compute.googleapis.com/BackendService"
+    "compute.googleapis.com/Disk"
+    "compute.googleapis.com/Firewall"
+    "compute.googleapis.com/ForwardingRule"
+    "compute.googleapis.com/Address"
+    "compute.googleapis.com/Instance"
+    "compute.googleapis.com/SslCertificate"
+    "compute.googleapis.com/TargetHttpsProxy"
+    "compute.googleapis.com/UrlMap"
+    "compute.googleapis.com/Snapshot"
+    "compute.googleapis.com/Network"
+    "compute.googleapis.com/TargetHttpProxy"
+    "compute.googleapis.com/MachineImage"
+    "compute.googleapis.com/InstanceGroup"
+    "compute.googleapis.com/InstanceTemplate"
+    "compute.googleapis.com/InstanceGroupManager"
+    "compute.googleapis.com/SslPolicy"
+    "compute.googleapis.com/Router"
+    "compute.googleapis.com/Image"
+    "compute.googleapis.com/VpnTunnel"
+    "compute.googleapis.com/TargetPool"
+    "compute.googleapis.com/HttpHealthCheck"
+    "compute.googleapis.com/ResourcePolicy"
+    "compute.googleapis.com/TargetVpnGateway"
+    "compute.googleapis.com/NetworkAttachment"
+    "compute.googleapis.com/SecurityPolicy"
+    "compute.googleapis.com/ServiceAttachment"
+    "compute.googleapis.com/VpnGateway"
+    "compute.googleapis.com/PacketMirroring"
+    "compute.googleapis.com/ExternalVpnGateway"
+    # "storage.googleapis.com/Bucket",
+    # "bigquery.googleapis.com/Dataset",
+    # "sqladmin.googleapis.com/Instance",
+    # "artifactregistry.googleapis.com/Repository",
     #"pubsub.googleapis.com/Topic",
-    "container.googleapis.com/Cluster",
+    # "container.googleapis.com/Cluster",
     # "container.googleapis.com/NodePool",
-    "secretmanager.googleapis.com/Secret",
+    # "secretmanager.googleapis.com/Secret",
     # "cloudresourcemanager.googleapis.com/Project",
-    "cloudkms.googleapis.com/CryptoKey",
+    # "cloudkms.googleapis.com/CryptoKey",
     # "apikeys.googleapis.com/Key",
-    "appengine.googleapis.com/Application",
-    "appengine.googleapis.com/Service",
-    "appengine.googleapis.com/Version",
-    "cloudfunctions.googleapis.com/Function",
+    # "appengine.googleapis.com/Application",
+    # "appengine.googleapis.com/Service",
+    # "appengine.googleapis.com/Version",
+    # "cloudfunctions.googleapis.com/Function",
 }
 
 SUPPORTED_TAG_RESOURCES = {
     # Populate only with officially supported
     # resource types after verification.
-    "pubsub.googleapis.com/Topic",
-    "compute.googleapis.com/ForwardingRule",
+    # "pubsub.googleapis.com/Topic",
+    # "compute.googleapis.com/ForwardingRule",
 }
